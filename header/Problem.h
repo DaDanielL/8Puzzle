8f41cc5b00752776ddb45079de15b33d98dfb25d
DaDanielL <danieldaolin@gmail.com>
2024-05-03 03:18:05 -0700

    added Problem.h

---
+++ header/Problem.h
@@ -0,0 +1,26 @@
+#ifndef PROBLEM_HPP
+#define PROBLEM_HPP
+
+#include <vector>
+using namespace std;
+
+class Problem{
+    private:
+        int size;
+        vector<vector<int>> initial_state;
+        vector<vector<int>> goal_state;
+
+    public:
+        Problem();
+        Problem(vector<std::vector<int>> initial_state);
+
+        vector<vector<int>> up(vector<std::vector<int>> curState);
+        vector<vector<int>> down(vector<std::vector<int>> curState);
+        vector<vector<int>> left(vector<std::vector<int>> curState);
+        vector<vector<int>> right(vector<std::vector<int>> curState);
+
+        int manhattanDist(vector<std::vector<int>> curState);
+        int euclidDist(vector<std::vector<int>> curState);
+};
+
+#endif
